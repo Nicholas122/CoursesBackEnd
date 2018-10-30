6e420ec71cb6cc9c4aec0cb1650ab9e83fc3a562
Nicholas Gritsuk <gritsuk57@gmail.com>
2018-10-30 11:35:00 +0200

    Changes

--- src/AppBundle/Controller/HomePageController.php
+++ src/AppBundle/Controller/HomePageController.php
@@ -5,6 +5,11 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomePageController extends Controller
 {
@@ -15,4 +20,56 @@ class HomePageController extends Controller
     {
         return $this->render('homepage/index.html.twig');
     }
+
+    /**
+     * @Route("/init", name="init")
+     */
+    public function initAction(KernelInterface $kernel)
+    {
+       $this->installCkeditor($kernel);
+       $this->installAssets($kernel);
+       $this->clearCache($kernel);
+
+        return new Response();
+    }
+
+    private function installCkeditor(KernelInterface $kernel)
+    {
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput(array('command' => 'ckeditor:install'));
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $content = $output->fetch();
+    }
+
+    private function installAssets(KernelInterface $kernel)
+    {
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput(array('command' => 'assets:install'));
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $content = $output->fetch();
+    }
+
+    private function clearCache(KernelInterface $kernel)
+    {
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput(array('command' => 'cache:clear'));
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $content = $output->fetch();
+    }
+
 }
